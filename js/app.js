8ff076f2976fbc018e893164f31c1a231e95dc46
Gyva <umwerechr97@gmail.com>
2023-08-10 00:15:06 +0200

    display country info

--- js/app.js
+++ js/app.js
@@ -5,33 +5,77 @@ const region = document.querySelectorAll(".region");
 const search1 = document.querySelector(".search1");
 const toggle = document.querySelector(".toggle");
 const moon = document.querySelector(".moon");
+const countryInfor = location.href?.split('?')
+let selectedCountry
+if(countryInfor.length > 1) {
+    selectedCountry = location.href?.split('?')?.[1]?.split('=')[1]?.replaceAll('%20', ' ')
+}
 
 async function getCountry() {
 
     const url = await fetch("https://restcountries.com/v3.1/all");
     const res = await url.json();
-    console.log(res);
-    res.forEach(element => {
-        showCountry(element)
+    if(selectedCountry) {
 
-    });
+    const data = res.find(country => country.name.official === selectedCountry)
+    const currencies = data.currencies
+    const languages =data.languages
+    const tld =data.tld
+    const name =data.name.nativeName
+    console.log(name);
+    console.log(Object.keys(name).map(dd => `<div><p>${name[dd].official}</p> <p>${name[dd].common}</p></div>`).join(''))
+
+    document.querySelector('.outer').innerHTML = ` <a href="/" ">
+   <button class="back"> <i class="fa fa-arrow-left-long"></i> Back</button>
+</a>
+    <div class="modal">
+        <div class="leftModal">
+            <img src="${data.flags.svg}" alt="">
+        </div>
+        <div class="rightModal">
+            <h1> ${data.name.official}</h1>
+           
+            <div class="modalInfo">
+                <div class="innerLeft inner">
+                    <p><strong>Native Name:</strong> ${Object.keys(name).map(dd => `${name[dd].official} ${name[dd].common}`).join(', ')}</p>
+                    <p><strong>Population:</strong> ${data.population}</p>
+                    <p><strong>Region:</strong> ${data.region}</p>
+                    <p><strong>Sub-region:</strong> ${data.subregion}</p>
+                    <p><strong>Capital:</strong> ${data.capital}</p>
+                </div>
+                <div class="innerRight inner">
+                    <p><strong>Top level Domain:</strong> ${Object.keys(tld).map(dd => `${tld[dd]}`).join(', ')}</p>
+                    <p>${Object.keys(currencies).reduce((currencyEl, currency)=> {
+                        return currencyEl + ` <strong>Currencies:</strong> ${currencies[currency].name}, ${currencies[currency].symbol} `
+                    }, '')}</p> 
+                    <p><strong>Languages:</strong> ${Object.keys(languages).map(dd => `${languages[dd]}`).join(', ')} </p> 
+                  
+                </div>
+            </div>
+        </div>
+    </div>`;
+    } else {
+
+        res.forEach(element => {
+            showCountry(element)
+        });
+    }
 }
 getCountry()
 
 function showCountry(data) {
-
     const country = document.createElement("div");
     country.classList.add("country")
 
-    country.innerHTML = ` <div class="country-img">
+    country.innerHTML = ` <a href="/?country=${data.name.official}"><div class="country-img">
     <img src="${data.flags.svg}" alt="">
 </div>
 <div class="country-info">
-    <h5 class= "countryName">${data.name.common}</h5>
+    <h5 class= "countryName">${data.name.official}</h5>
     <p><strong>Population:</strong> ${data.population}</p>         
     <p class="regionName"><strong>Region:</strong> ${data.region}</p>         
     <p><strong>Capital:</strong> ${data.capital}</p>                 
-</div>`;
+</div></a>`;
 
     countriesElem.appendChild(country)
     country.addEventListener("click", () => {
@@ -64,9 +108,9 @@ region.forEach(element => {
 search1.addEventListener("input", () => {
     Array.from(countryName).forEach(elem => {
         if (elem.innerText.toLowerCase().includes(search1.value.toLowerCase())) {
-            elem.parentElement.parentElement.style.display = "grid"
+            elem.closest('.country').style.display = "grid"
         } else {
-            elem.parentElement.parentElement.style.display = "none"
+            elem.closest('.country').style.display = "none"
         }
     });
 })
@@ -80,29 +124,7 @@ toggle.addEventListener("click", () => {
 const countryModal = document.querySelector(".countryModal");
 function showCountryDetail(data) {
     countryModal.classList.toggle("show")
-    countryModal.innerHTML = `<button class="back">Back</button>
-    <div class="modal">
-        <div class="leftModal">
-            <img src="${data.flags.svg}" alt="">
-        </div>
-        <div class="rightModal">
-            <h1> ${data.name.common}</h1>
-            <div class="modalInfo">
-                <div class="innerLeft inner">
-                    <p><strong>Native Name:</strong></p>
-                    <p><strong>Population:</strong> ${data.population}</p>
-                    <p><strong>Region:</strong> ${data.region}</p>
-                    <p><strong>Sub-region:</strong> ${data.subregion}</p>
-                </div>
-                <div class="innerRight inner">
-                    <p><strong>Capital:</strong> ${data.capital}</p>
-                    <p><strong>Top level Domain:</strong></p>
-                    <p><strong>Currencies:</strong></p> 
-                    <p><strong>Languages:</strong> ${data.languages}</p> 
-                </div>
-            </div>
-        </div>
-    </div>`
+    const currencies = data.currencies
 
     const back = countryModal.querySelector(".back")
     back.addEventListener("click", () => {
