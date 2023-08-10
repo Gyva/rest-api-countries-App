e0f97f0d3bfd228d9ddfd8778f4a5270c1e7ab63
Gyva <umwerechr97@gmail.com>
2023-08-10 12:13:30 +0200

    store color mode in locastorage

--- js/app.js
+++ js/app.js
@@ -7,25 +7,28 @@ const toggle = document.querySelector(".toggle");
 const moon = document.querySelector(".moon");
 const countryInfor = location.href?.split('?')
 let selectedCountry
-if(countryInfor.length > 1) {
+if (countryInfor.length > 1) {
     selectedCountry = location.href?.split('?')?.[1]?.split('=')[1]?.replaceAll('%20', ' ')
 }
-
+if (localStorage.getItem("mode") == undefined) {
+    localStorage.setItem('mode', "light");
+}
+changeMode(localStorage.getItem("mode"))
 async function getCountry() {
 
     const url = await fetch("https://restcountries.com/v3.1/all");
     const res = await url.json();
-    if(selectedCountry) {
+    if (selectedCountry) {
 
-    const data = res.find(country => country.name.official === selectedCountry)
-    const currencies = data.currencies
-    const languages =data.languages
-    const tld =data.tld
-    const name =data.name.nativeName
-    console.log(name);
-    console.log(Object.keys(name).map(dd => `<div><p>${name[dd].official}</p> <p>${name[dd].common}</p></div>`).join(''))
+        const data = res.find(country => country.name.official === selectedCountry)
+        const currencies = data.currencies
+        const languages = data.languages
+        const tld = data.tld
+        const name = data.name.nativeName
+        console.log(name);
+        console.log(Object.keys(name).map(dd => `<div><p>${name[dd].official}</p> <p>${name[dd].common}</p></div>`).join(''))
 
-    document.querySelector('.outer').innerHTML = ` <a href="/" ">
+        document.querySelector('.outer').innerHTML = ` <a href="https://gyva.github.io/rest-api-countries-App">
    <button class="back"> <i class="fa fa-arrow-left-long"></i> Back</button>
 </a>
     <div class="modal">
@@ -45,9 +48,9 @@ async function getCountry() {
                 </div>
                 <div class="innerRight inner">
                     <p><strong>Top level Domain:</strong> ${Object.keys(tld).map(dd => `${tld[dd]}`).join(', ')}</p>
-                    <p>${Object.keys(currencies).reduce((currencyEl, currency)=> {
-                        return currencyEl + ` <strong>Currencies:</strong> ${currencies[currency].name}, ${currencies[currency].symbol} `
-                    }, '')}</p> 
+                    <p>${Object.keys(currencies).reduce((currencyEl, currency) => {
+            return currencyEl + ` <strong>Currencies:</strong> ${currencies[currency].name}, ${currencies[currency].symbol} `
+        }, '')}</p> 
                     <p><strong>Languages:</strong> ${Object.keys(languages).map(dd => `${languages[dd]}`).join(', ')} </p> 
                   
                 </div>
@@ -67,7 +70,7 @@ function showCountry(data) {
     const country = document.createElement("div");
     country.classList.add("country")
 
-    country.innerHTML = ` <a href="/?country=${data.name.official}"><div class="country-img">
+    country.innerHTML = ` <a href="https://gyva.github.io/rest-api-countries-App/?country=${data.name.official}"><div class="country-img">
     <img src="${data.flags.svg}" alt="">
 </div>
 <div class="country-info">
@@ -115,12 +118,23 @@ search1.addEventListener("input", () => {
     });
 })
 
-toggle.addEventListener("click", () => {
-    document.body.classList.toggle("dark")
-    moon.classList.toggle("fas")
+toggle.addEventListener("click",()=>{
+    const mode = localStorage.getItem("mode")=="dark"?
+    "light":"dark";
+changeMode(mode);
 })
 
-
+function changeMode (mode) {
+    if(mode == "dark") {
+        document.body.classList.toggle("dark")
+        moon.classList.toggle("fas")
+    }
+    else{
+        document.body.classList.remove("dark")
+        moon.classList.remove("fas") 
+    } 
+    localStorage.setItem("mode",mode)
+}
 const countryModal = document.querySelector(".countryModal");
 function showCountryDetail(data) {
     countryModal.classList.toggle("show")
